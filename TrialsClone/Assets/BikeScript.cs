a29244b65a9a8923afd8f57d122f95bd20d476f3
SteveRodkiss <srodkiss@rosmini.school.nz>
2015-07-03 19:52:47 +1200

    Bike control
    Got bike control working and visual wheel. Drawn a basic wheel

---
+++ TrialsClone/Assets/BikeScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeScript : MonoBehaviour
+{
+
+	public float MotorPower = 500f;
+	public WheelCollider RearWheel;
+	public WheelCollider FrontWheel;
+	public Transform FrontWheelMesh;
+	public Transform RearWheelMesh;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float torque = Input.GetAxis("Horizontal") * MotorPower;
+		RearWheel.motorTorque = torque;
+
+		ApplyVisualTransform(RearWheel,RearWheelMesh);
+		ApplyVisualTransform(FrontWheel,FrontWheelMesh);
+	}
+
+	void ApplyVisualTransform(WheelCollider wc, Transform mesh)
+	{
+		Vector3 pos;
+		Quaternion rot;
+		wc.GetWorldPose(out pos,out rot);
+		mesh.position = pos;
+		mesh.rotation = rot;
+	}
+
+
+}
---
+++ TrialsClone/Assets/CameraFollowScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour
+{
+
+	public GameObject Target;
+	public Vector3 Offset = new Vector3(20f,10f,0f);
+
+	// Use this for initialization
+	void Start ()
+	{
+		Target = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Target.transform.position + Offset;
+		transform.LookAt(Target.transform.position);
+	}
+}
